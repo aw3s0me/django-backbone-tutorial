f082827c294a5234cc0ca648a1858244aa35398b
Aleksander Korovin <aw3s0me@rocketmail.com>
2014-04-03 14:46:51 +0200

    Added admin panel

--- polls/admin.py
+++ polls/admin.py
@@ -1,6 +1,34 @@
 from django.contrib import admin
 from polls.models import Question
+from polls.models import Choice
 
-admin.site.register(Question)
+#Here we define the way how we wanna see fields
+
+#class ChoiceInline(admin.StackedInline):
+class ChoiceInline(admin.TabularInline):
+	model = Choice
+	extra = 3
+
+
+#1Version
+#class PollAdmin(admin.ModelAdmin):
+#	fields = ['pub_date', 'question_text'] #just changed the order
+
+#2Version. Divide fields
+class PollAdmin(admin.ModelAdmin):
+	fieldsets = [
+		(None,               {'fields': ['question_text']}),
+        ('Date information', {'fields': ['pub_date'], 'classes': ['collapse']}),
+	]
+	inlines = [ChoiceInline]
+	list_display = ('question_text', 'pub_date', 'isPublished') #In the list we output all information
+
+
+#register as default way
+#admin.site.register(Question)
+
+#register model in a new way
+admin.site.register(Question, PollAdmin)
+admin.site.register(Choice)
 
 # Register your models here.
